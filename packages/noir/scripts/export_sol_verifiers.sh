f67525bb16cb9b5a3e28b07594723b95533c41c5
filip <filip@kryha.io>
2023-10-27 17:51:13 +0200

    feat: added noir template

---
+++ packages/noir/scripts/export_sol_verifiers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if ! command -v nargo &> /dev/null
+then
+    echo "nargo could not be found, exiting..."
+    echo ""
+    exit 2
+fi
+
+echo "exporting sol verifiers..."
+mkdir ../hardhat/contracts/verifiers
+rm ../hardhat/contracts/verifiers/*
+
+for d in circuits/*/ ; do
+  echo $d
+  cd $d
+  nargo codegen-verifier
+  mv contract/$(basename $d)/plonk_vk.sol ../../../hardhat/contracts/verifiers/$(basename $d).sol
+  cd ../..
+done
