f4cde15c97d955c4533118b5b3fcec718ee14058
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-28 22:07:36 +0100

    Added hooks without implimentation
    Please put the logic for the interaction with aztec in these hooks

---
+++ packages/nextjs/hooks/aztec/useAztecContract.tsx
@@ -0,0 +1,6 @@
+// Put the interactions here to connect to the Aztec contract
+// Maybe also make this hook universal so you can use it for different contracts
+
+export const useAztecContract = () => {
+  return {};
+};
---
+++ packages/nextjs/hooks/aztec/useAztecSandbox.tsx
@@ -0,0 +1,5 @@
+// Put the stuff here to connect to the Sandbox
+
+export const useAztecSandbox = () => {
+  return {};
+};
